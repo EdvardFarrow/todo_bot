fb904637b31123aed46bd67cd8c748a16bda6224
edfarrow <edvardfrrw@gmail.com>
2025-12-16 20:46:30 +0400

    feat(tasks): define initial database schema for tasks

---
+++ backend/apps/tasks/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.conf import settings
+from apps.core.models import SnowflakeModel
+
+class Category(SnowflakeModel):
+    name = models.CharField("Name", max_length=100)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name="categories",
+        verbose_name="Owner"
+    )
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        unique_together = [('name', 'user')] 
+
+    def __str__(self) -> str:
+        return self.name
+
+class Task(SnowflakeModel):
+    title = models.CharField("Title", max_length=255)
+    description = models.TextField("Description", blank=True)
+    deadline = models.DateTimeField("Deadline", null=True, blank=True)
+    is_completed = models.BooleanField("Completed", default=False)
+    
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="tasks"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="tasks"
+    )
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return self.title
